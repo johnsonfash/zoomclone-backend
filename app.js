0ef23bfbc9c8caf33234cd60188b653274ccbb20
johnsonfash <tosin.fashanu@supermartng.com>
2022-03-16 16:41:17 +0100

    edited project port

--- app.js
+++ app.js
@@ -24,4 +24,5 @@ io.on('connection', (socket) => {
   })
 })
 
-server.listen(5000, () => console.log('server started at 5000'));
+let port = process.env.PORT || 5000;
+server.listen(port, () => console.log('server started at port: ' + port));
