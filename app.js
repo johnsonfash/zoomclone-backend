752b7f9a1b69c9d48c730d970c54240ece1363a3
johnsonfash <tosin.fashanu@supermartng.com>
2022-03-17 14:54:54 +0100

    commit

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
-const io = require('socket.io')(server, { serveClient: false, origins: '*', cors: { origin: '*' } });
+// const io = require('socket.io')(server, { serveClient: false, origins: '*', cors: { origin: '*' } });
+const io = require('socket.io')(server, { origins: '*', cors: { origin: '*' } });
 const { ExpressPeerServer } = require('peer');
 const peerServer = ExpressPeerServer(server, { debug: true });
-
 
 
 if (process.env.NODE_ENV === 'production') {
