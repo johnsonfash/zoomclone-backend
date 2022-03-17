21f868371325f84bcc7ad7d7b1ef74c595874ba5
johnsonfash <tosin.fashanu@supermartng.com>
2022-03-17 19:22:34 +0100

    .

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-// var cors = require('cors');
-// app.use(cors());
+var cors = require('cors');
+app.use(cors());
 const server = require('http').Server(app);
 // const io = require('socket.io')(server, { serveClient: false, origins: '*', cors: { origin: '*' } });
 const io = require('socket.io')(server);
