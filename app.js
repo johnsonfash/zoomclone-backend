b7487bff35136ed16cd6680946f30fc3ea6a84a9
johnsonfash <tosin.fashanu@supermartng.com>
2022-03-17 17:03:45 +0100

    .

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use('/peerjs', peerServer);
 io.on('connection', (socket) => {
   socket.on("join-room", ({ roomId, peerID, userID, userName }) => {
     socket.join(roomId);
-    socket.to(roomId).emit('userConnected', { peerID, userID, userName });
+    io.to(roomId).emit('userConnected', { peerID, userID, userName });
     socket.on('message', (obj) => {
       io.to(roomId).emit('newMessage', obj)
     });
