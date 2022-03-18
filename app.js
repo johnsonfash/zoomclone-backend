a0d1be9be0b718bd34bac0e4b0018f6a78719e5d
johnsonfash <tosin.fashanu@supermartng.com>
2022-03-18 08:26:25 +0100

    h

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ if (process.env.NODE_ENV === 'production') {
   })
 }
 
-app.use(cors());
+app.use(cors({origin: 'https://zuum.herokuapp.com'}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
